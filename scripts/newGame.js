dcb9fa15db092383a0001eec5dbac21b31ae849a
Daniel Quintino <danieljucas123@gmail.com>
2023-11-23 19:42:12 -0300

    update grid columns

--- scripts/newGame.js
+++ scripts/newGame.js
@@ -1,25 +1,22 @@
 const inputName = document.querySelector(".inputName");
 const newGameButton = document.querySelector(".newGameButton");
-const newGameForm = document.querySelector(".newGameForm")
+const newGameForm = document.querySelector(".newGameForm");
 
 function validateInput(event) {
-    if (event.target.value.length >= 3) {
-        newGameButton.removeAttribute("disabled")
-    } else {
-        newGameButton.setAttribute("disabled", "true");
-    }
-      
-} 
+  if (event.target.value.length >= 3) {
+    newGameButton.removeAttribute("disabled");
+  } else {
+    newGameButton.setAttribute("disabled", "true");
+  }
+}
 
 function handleSubmitNewGame(event) {
-    event.preventDefault();
-    console.log(inputName.value);
-    localStorage.setItem("@memoryGame:playerName",
-    inputName.value);
-    inputName.value = "";
-    window.location.href = "pages/cards.html"
+  event.preventDefault();
+  console.log(inputName.value);
+  localStorage.setItem("@memoryGame:playerName", inputName.value);
+  inputName.value = "";
+  window.location.href = "pages/cards.html";
 }
 
 inputName.addEventListener("input", validateInput);
 newGameForm.addEventListener("submit", handleSubmitNewGame);
-
